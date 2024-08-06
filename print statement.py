9ce1674d4b4001ffc75300f01bf0a9a49b78ee40
midhunsai04 <164469674+midhunsai04@users.noreply.github.com>
2024-08-06 20:15:10 +0530

    Add files via upload

---
+++ print statement.py
@@ -0,0 +1,6 @@
+print("""
+Midhun
+august 6th 2024
+I am a aspiring computer science engineer with a lot of passion for exploring various techniques and advanced technologies that are currently taking over the world
+""")
+print("i am excited")
